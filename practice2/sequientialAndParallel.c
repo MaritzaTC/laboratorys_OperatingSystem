93d3b825a08257215fbec72d0a1cc9724e3f0717
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-14 19:22:26 -0500

    - Changed the number of processes from 4 to 1 to simplify execution. - Updated matrix file names from "A_small.txt" and "B_small.txt" to "A-big.txt" and "B_big.txt" for testing with larger datasets.

--- practice2/sequientialAndParallel.c
+++ practice2/sequientialAndParallel.c
@@ -10,7 +10,7 @@
 #include <unistd.h>
 #include <time.h>
 
-#define NUM_PROCESSES 4
+#define NUM_PROCESSES 1
 
 void readMatrix(const char *filename, double *matrix, int rows, int cols) {
     FILE *fp = fopen(filename, "r");
@@ -125,8 +125,8 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    readMatrix("A_small.txt", A, N, M);
-    readMatrix("B_small.txt", B, M, P);
+    readMatrix("A-big.txt", A, N, M);
+    readMatrix("B_big.txt", B, M, P);
 
     // Secuencial
     clock_t start_seq = clock();
